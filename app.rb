0a97aa775cb9878326a17ce31b562638ee1c1795
Les Aker <me@lesaker.org>
2021-03-23 19:20:20 -0400

    update setup

--- app.rb
+++ app.rb
@@ -9,8 +9,7 @@ set :public_folder, File.dirname(__FILE__) + '/static'
 
 client = Plaid::Client.new(env: ENV['PLAID_ENV'],
                            client_id: ENV['PLAID_CLIENT_ID'],
-                           secret: ENV['PLAID_SECRET'],
-                           public_key: ENV['PLAID_PUBLIC_KEY'])
+                           secret: ENV['PLAID_SECRET'])
 
 # We store the access_token in memory - in production, store it in a secure
 # persistent data store.
